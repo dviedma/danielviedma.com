e13f87cf834b0a209e34fb6d47ab995d79ea5452
Daniel Viedma <daniel@Daniels-MBP.fios-router.home>
2019-06-25 17:10:29 -0400

    adding husky pre commit hook

--- src/templates/project/index.js
+++ src/templates/project/index.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React from "react"
 
 import Layout from "@layouts/layout"
 
 class ProjectTemplate extends React.Component {
-
   // Render
   render() {
     // Alias data to props
-    const { ...data } = { ...this.props.data.contentfulProject };
+    const { ...data } = { ...this.props.data.contentfulProject }
 
-    return (<Layout><h1>{data.title}</h1></Layout>);
+    return (
+      <Layout>
+        <h1>{data.title}</h1>
+      </Layout>
+    )
   }
 }
 
-export default ProjectTemplate;
+export default ProjectTemplate
 
 export const pageQuery = graphql`
   query($slug: String!) {
@@ -21,4 +24,4 @@ export const pageQuery = graphql`
       title
     }
   }
-`;
+`
