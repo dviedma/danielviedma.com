916b14f9b73d6924f2ed77c69da5a0549df2c6c9
dviedma <dviedm@gmail.com>
2022-08-21 22:03:18 -0700

    removing references to allContentfulProject

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,22 +8,4 @@ const slash = require(`slash`)
 // create pages.
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-
-  // The “graphql” function allows us to run arbitrary
-  // queries against the local Contentful graphql schema. Think of
-  // it like the site has a built-in database constructed
-  // from the fetched data that you can run queries against.
-  const contentfulData = await graphql(
-    `
-      query {
-        allContentfulProject {
-          nodes {
-            slug
-          }
-        }
-      }
-    `
-  ).catch(e => {
-    throw e
-  })
 }
